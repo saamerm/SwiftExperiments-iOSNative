323e07ffa74a6aac680b4cd10f295e934e7dc2ec
Saamer Mansoor <i@saamer.me>
2019-03-06 20:15:23 -0500

    No changes
    Basic app with Tests and No core data, No changes made

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Saamer Mansoor on 2019-03-06.
+//  Copyright © 2019 Saamer Mansoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
